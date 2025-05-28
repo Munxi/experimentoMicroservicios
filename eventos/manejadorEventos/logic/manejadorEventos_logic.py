fdc5368840c5b441165d50af6ae3568b89f8db3f
Felipe Sales <l.sales@uniandes.edu.co>
2025-05-27 23:57:06 -0500

    Verificacion doctor en evento

--- eventos/manejadorEventos/logic/manejadorEventos_logic.py
+++ eventos/manejadorEventos/logic/manejadorEventos_logic.py
@@ -1,16 +1,31 @@
+import requests
+from django.core.exceptions import ValidationError
 from ..models import Evento
 from informacionPaciente.models import Paciente
 
+
+def verificar_doctor(doctor_id):
+    try:
+        response = requests.get(f"http://10.128.0.85:8080/doctores/{doctor_id}")
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
 def create_evento(evento_data):
-        paciente_ni = evento_data.get('paciente_ni')
-        paciente = Paciente.objects.get(NI=paciente_ni)
-        evento = Evento(
-            paciente=paciente,
-            tipo=evento_data['tipo'],
-            doctorEncargado=evento_data['doctorEncargado'],
-            resultado=evento_data.get('resultado'),
-            fecha=evento_data['fecha'],
-            diagnostico=evento_data.get('diagnostico'),
-        )
-        evento.save()
-        return evento
+    
+    doctor_id = evento_data.get('doctorEncargado')
+    if not doctor_id or not verificar_doctor(doctor_id):
+        raise ValueError(f"Doctor con ID {doctor_id} no encontrado")
+    
+    paciente_ni = evento_data.get('paciente_ni')
+    paciente = Paciente.objects.get(NI=paciente_ni)
+    evento = Evento(
+        paciente=paciente,
+        tipo=evento_data['tipo'],
+        doctorEncargado=evento_data['doctorEncargado'],
+        resultado=evento_data.get('resultado'),
+        fecha=evento_data['fecha'],
+        diagnostico=evento_data.get('diagnostico'),
+    )
+    evento.save()
+    return evento
--- eventos/manejadorEventos/views.py
+++ eventos/manejadorEventos/views.py
@@ -1,13 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
-
-from .logic import manejadorEventos_logic as el
+from .logic import manejadorEventos_logic
 from django.core import serializers
+import json
+import requests
+from django.conf import settings
 
 @csrf_exempt
 def evento_view(request):
     if request.method == 'POST':
-        evento_dto = el.create_evento(json.loads(request.body))
-        evento = serializers.serialize('json', [evento_dto, ])
-        return HttpResponse(evento, 'application/json')
+        try:
+            data = json.loads(request.body)
+            
+            #doctor
+            doctor_resp = requests.get(f"{settings.DOCTORES_MS_URL}/{data['doctorEncargado']}")
+            if doctor_resp.status_code != 200:
+                return JsonResponse({"error": "El doctor no existe"}, status=400)
+
+            # paciente
+            paciente_resp = requests.get(f"{settings.PACIENTES_MS_URL}/{data['paciente_ni']}")
+            if paciente_resp.status_code != 200:
+                return JsonResponse({"error": "El paciente no eixste"}, status=400)
+
+            evento_dto = manejadorEventos_logic.create_evento(data)
+            evento = serializers.serialize('json', [evento_dto, ])
+            return HttpResponse(evento, 'application/json', status=201)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
